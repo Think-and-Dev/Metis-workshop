f135a7a033e9bacd228df5e5f7062c4977b5849a
Ayrton Paredes <ayrton@thinkanddev.com>
2023-05-23 14:21:02 -0400

    Stats page

--- dapp/src/pages/[electionId]/stats.tsx
+++ dapp/src/pages/[electionId]/stats.tsx
@@ -20,10 +20,20 @@ const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, per
     );
 };
 
+const CrownSvg = () => {
+    return <svg fill='yellow' height="24px" width="24px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512" >
+        <polygon points="0,443.733 0,68.267 17.067,68.267 136.533,187.733 256,68.267 375.467,187.733 
+   494.933,68.267 512,68.267 512,443.733 "/>
+        <polygon points="494.933,68.267 375.467,187.733 256.002,68.267 256,68.267 256,443.733 512,443.733 
+   512,68.267 "/>
+    </svg>
+}
+
 const Stats: NextPage = () => {
     const router = useRouter();
     const electionId = parseInt(router.query.electionId as string);
-    const { candidates, getCandidateVotes } = useVoteContract({ electionId })
+    const { candidates, getCandidateVotes, isElectionActive } = useVoteContract({ electionId })
     const votesA = getCandidateVotes({ electionId, candidateAddress: candidates[0] })
     const votesB = getCandidateVotes({ electionId, candidateAddress: candidates[1] })
 
@@ -32,29 +42,60 @@ const Stats: NextPage = () => {
         { name: candidates[1], value: votesB }
     ]
 
-    return <div className='w-screen h-[800px] overflow-hidden flex justify-center items-center '>
-        <ResponsiveContainer width="100%" height="100%">
-            <PieChart width={800} height={800} style={{ transform: 'scale(2)' }}>
-                <Pie
-                    data={votes}
-                    cx="50%"
-                    cy="50%"
-                    labelLine={false}
-                    outerRadius={80}
-                    label={renderCustomizedLabel}
-                    fill="#8884d8"
-                    dataKey="value"
-                    height={800}
-                    width={800}
-                >
-                    {votes?.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                    ))}
-                </Pie>
-                <Tooltip />
-            </PieChart>
-        </ResponsiveContainer>
-    </div>
+    if (!isElectionActive?.data) return <>
+        <div className="flex">
+            <div className="modal-box">
+                <h3 className="font-bold text-lg">Election {electionId} hasn't started yet</h3>
+                <p className="py-4">Please come back when the timer end!</p>
+                <div className="modal-action">
+                    <label htmlFor="my-modal" className="btn" onClick={() => router.push(`/${electionId}`)}>Yay!</label>
+                </div>
+            </div>
+        </div>
+    </>
+
+    return <>
+        <h2 className='mt-16 text-center text-white font-bold text-5xl'>Status of Election {electionId}</h2>
+        <div className='w-screen h-[800px] overflow-hidden flex justify-center items-center '>
+            <ResponsiveContainer width="100%" height="100%">
+                <PieChart width={800} height={800} style={{ transform: 'scale(2)' }}>
+                    <Pie
+                        data={votes}
+                        cx="50%"
+                        cy="50%"
+                        labelLine={false}
+                        outerRadius={80}
+                        label={renderCustomizedLabel}
+                        fill="#8884d8"
+                        dataKey="value"
+                        height={800}
+                        width={800}
+                    >
+                        {votes?.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
+                    <Tooltip />
+                </PieChart>
+            </ResponsiveContainer>
+        </div>
+        <div className='w-full text-center flex justify-center'>
+            <ul className='flex flex-col gap-2'>
+                <li className='flex flex-row gap-2'>
+                    <p className='text-white text-xl'>Candidate address <span className='font-bold'>{candidates[0]}</span> : {votesA}</p>
+                    {
+                        votesA > votesB ? <CrownSvg /> : <></>
+                    }
+                </li>
+                <li className='flex flex-row gap-2'>
+                    <p className='text-white text-xl'>Candidate address <span className='font-bold'>{candidates[1]}</span> : {votesB}</p>
+                    {
+                        votesB > votesA ? <CrownSvg /> : <></>
+                    }
+                </li>
+            </ul>
+        </div >
+    </>
 }
 
 export default Stats;
